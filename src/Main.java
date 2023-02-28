7aa61e480b609a2d277cb15b20783e51c9c93979
crashing <leo2872497@gmail.com>
2023-02-28 18:06:56 +0400

    homework

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+
+
+/*
+Незаполненный прямоугольник
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1(){
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+    }
+    public static void task2(){
+        System.out.println("Task 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName.toUpperCase() + " " + firstName.toUpperCase() + " " + middleName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+    }
+    public static void task3(){
+        System.out.println("Task 3");
+        System.out.println("Task 2");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        firstName = firstName.replace("ё", "e");
+        middleName = middleName.replace("ё", "e");
+        String fullName = lastName.toUpperCase() + " " + firstName.toUpperCase() + " " + middleName.toUpperCase();
+        System.out.println(fullName);
+
+
+
+    }
+}
+
